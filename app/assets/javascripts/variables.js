a0767ab97d281fc28280b08e662ceb26236807ef
Martin Acevedo <macevedo@web-experto.com.ar>
2018-03-13 01:14:43 -0300

    [WIP] Agregar objetivos a variable

--- app/assets/javascripts/variables.js
+++ app/assets/javascripts/variables.js
@@ -1,5 +1,16 @@
+/* globals idx */
 function changeSeparator (idInput) {
 
   jQuery("#" + idInput).val(jQuery("#" + idInput).val().replace(",", "."));
+
+}
+
+function add_objective(){
+
+    idx += 1;
+    // jQuery('#usuario_id').val();
+    var newTab = jQuery("[id^='tab']").clone();
+    jQuery(newTab).find(".panel.panel-default.accordion-container").attr("id", "tab_" + idx)
+    jQuery('#accordion .accordion-container:last-child').after(newTab);
 
 }
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,11 @@
 module UsersHelper
-  def display_user_selector
-    select('usuario', 'id', User.sucursal.collect { |u| ["#{u.nombre.capitalize} #{u.apellido.capitalize}", u.id] }, { include_blank: false }, class: 'form-control input-height', onchange: 'hideObjetivosForm();')
+  def display_user_selector(variable, condition)
+    users = if condition == :exclude
+              User.sucursal.where.not(id: variable.objetivos.map(&:user_id))
+            else
+              User.sucursal
+            end
+
+    select('usuario', 'id', users.collect { |u| ["#{u.nombre.capitalize} #{u.apellido.capitalize}", u.id] }, { include_blank: false }, class: 'form-control input-height')
   end
 end
