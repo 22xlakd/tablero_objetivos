cd35bd8c2f32d9c9ddc7807ec85c90e00aa058ff
Martin Acevedo <macevedo@web-experto.com.ar>
2018-03-21 20:18:49 -0300

    Fix error al borrar user con objetivos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -72,6 +72,7 @@ class UsersController < ApplicationController
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    @user.objetivos.map(&:destroy)
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: I18n.t(:user_destroyed) }
