bd67189d5482607133c8c433da0f86b2e52602e7
Martin Acevedo <macevedo@web-experto.com.ar>
2018-09-04 00:59:50 -0300

    [WIP] Fix de agregar objetivos

--- app/assets/javascripts/variables.js
+++ app/assets/javascripts/variables.js
@@ -90,38 +90,44 @@ function add_objective(){
         parentSelector = '.accordion-container:last-child'
     }
     idx += 1;
+    if(jQuery(newTab).find(".row").length > 1){
+        jQuery(newTab).find(".row").first().remove()
+    }
     jQuery(newTab).attr("id", "tab_" + idx)
     jQuery(newTab).removeClass("hidden")
     jQuery(newTab).find(".panel-heading.accordion-header").attr("id", "heading_" + idx)
     jQuery(newTab).find(".lnk-username").attr("href", "#collapse_" + idx)
     jQuery(newTab).find(".lnk-username").attr("aria-controls", "collapse_" + idx)
     var input_obj_id = jQuery(newTab).find(".lnk-username input[type='hidden']")
-    input_obj_id.attr("id", "variable_objetivos_attributes_" + idx  + "_id")
-    input_obj_id.attr("name", "variable[objetivos_attributes][" + idx  + "][id]")
+    input_obj_id.attr("id", "variable_objetivos_attributes_" + idx2  + "_id")
+    input_obj_id.attr("name", "variable[objetivos_attributes][" + idx2  + "][id]")
     input_obj_id.val('')
-    var input_usr_id = '<input value="' + jQuery('#usuario_id').val() + '" type="hidden" name="variable[objetivos_attributes][' + idx + '][user_id]" id="variable_objetivos_attributes_' + idx + '_user_id" />'
+    //var input_usr_id = '<input value="' + jQuery('#usuario_id').val() + '" type="hidden" name="variable[objetivos_attributes][' + idx2 + '][user_id]" id="variable_objetivos_attributes_' + idx2 + '_user_id" />'
     jQuery(newTab).find(".lnk-username").html(jQuery('#usuario_id option:selected').text())
-    jQuery(newTab).find(".lnk-username").append( input_obj_id)
-    jQuery(newTab).find(".lnk-username").append( input_usr_id)
+    //jQuery(newTab).find(".lnk-username").append( input_obj_id)
+    //jQuery(newTab).find(".lnk-username").append( input_usr_id)
     jQuery(newTab).find(".collapse").attr("id","collapse_" + idx)
     jQuery(newTab).find(".collapse").attr("aria-labelledby","heading_" + idx)
+    jQuery(newTab).find("#variable_objetivos_attributes_"+idx2+"_user_id").val(jQuery('#usuario_id').val())
 
     jQuery(newTab).find("label").each(function(){
-       jQuery(this).attr("for", jQuery(this).attr("for").replace(/[0-9]+/g, idx))
+       jQuery(this).attr("for", jQuery(this).attr("for").replace(/[0-9]+/g, idx2))
     });
 
     jQuery(newTab).find("select").each(function(){
-       jQuery(this).attr("id", jQuery(this).attr("id").replace(/[0-9]+/g, idx))
-       jQuery(this).attr("name", jQuery(this).attr("name").replace(/[0-9]+/g, idx))
+       jQuery(this).attr("id", jQuery(this).attr("id").replace(/[0-9]+/g, idx2))
+       jQuery(this).attr("name", jQuery(this).attr("name").replace(/[0-9]+/g, idx2))
        jQuery(this).val(jQuery(this)[0].options[0].value)
     });
     
     jQuery(newTab).find("input[type!='hidden']").each(function(){
-       jQuery(this).attr("id", jQuery(this).attr("id").replace(/[0-9]+/g, idx))
-       jQuery(this).attr("name", jQuery(this).attr("name").replace(/[0-9]+/g, idx))
+       jQuery(this).attr("id", jQuery(this).attr("id").replace(/[0-9]+/g, idx2))
+       jQuery(this).attr("name", jQuery(this).attr("name").replace(/[0-9]+/g, idx2))
        jQuery(this).val("")
     });
 
+    jQuery("#btn_date_obj_"+idx).unbind('click')
+    jQuery("#btn_date_obj_"+idx).bind('click', function(){ addDateObjetive(idx); });
     jQuery(parentSelector).after(newTab);
     jQuery("#collapse_" + idx).collapse('show');
     jQuery('#addObjectiveToUser').modal('hide');
